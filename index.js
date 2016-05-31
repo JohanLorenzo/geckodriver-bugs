9bf0c66ad1c74005155b071ff3a19a8fa8ea8ffb
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-31 11:11:53 +0200

    WIP

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const webdriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
 const firefoxCapabilities = require('selenium-webdriver/lib/capabilities')
   .Capabilities.firefox();
 const assert = require('assert');
 
+const profile = new firefox.Profile();
+profile.setAcceptUntrustedCerts(true);
+
+const options = new firefox.Options();
+options.setProfile(profile);
+
 firefoxCapabilities.set('marionette', true);
 const driverBuilder = new webdriver.Builder()
-  .withCapabilities(firefoxCapabilities);
+  .withCapabilities(firefoxCapabilities)
+  .setFirefoxOptions(options);
 
 driver = driverBuilder.build();
 
 describe('timeouts', function() {
+  this.timeout(30000); // in ms
   before(function() {
-    return driver.manage().timeouts().setScriptTimeout(20000);
-  });
+    return driver.get('https://localhost:8000');
+  })
 
-  it('should not fail', function() {
-    assert(true);
+  it('should not fail', function(done) {
+    setTimeout(done, 10000)
+    // assert(true);
   })
 
 })
