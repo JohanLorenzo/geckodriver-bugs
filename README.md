ace81a91a487f3a31c066db9e5cb89b3d7e78de9
Johan Lorenzo <jlorenzo@mozilla.com>
2016-06-07 15:14:31 +0200

    Tested against Chrome and Selenium Driver

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Repo to expose steps to reproduce between Selenium and GeckoDriver
 
 ## Usage
+Edit `package.json` to point to a local copy of selenium client
 ```sh
 npm install
 npm test
--- index.js
+++ index.js
@@ -1,21 +1,62 @@
 const webdriver = require('selenium-webdriver');
 const firefoxCapabilities = require('selenium-webdriver/lib/capabilities')
   .Capabilities.firefox();
-const assert = require('assert');
+const assert = require('chai').assert;
 
 firefoxCapabilities.set('marionette', true);
-const driverBuilder = new webdriver.Builder()
-  .withCapabilities(firefoxCapabilities);
 
-driver = driverBuilder.build();
+const browsers = [{
+    name: 'firefox',
+    builder: new webdriver.Builder()
+      .withCapabilities(firefoxCapabilities),
+  }, {
+    name: 'chrome',
+    builder: new webdriver.Builder()
+      .forBrowser('chrome'),
+  }, {
+    name: 'chrome through selenium server',
+    builder: new webdriver.Builder()
+      .forBrowser('chrome')
+      .usingServer('http://localhost:4444/wd/hub'),
+  }];
 
-describe('timeouts', function() {
-  before(function() {
-    return driver.manage().timeouts().setScriptTimeout(20000);
-  });
 
-  it('should not fail', function() {
-    assert(true);
+browsers.forEach(function(browser) {
+  driver = browser.builder.build();
+
+  describe(browser.name, function() {
+    this.timeout(10000);
+
+    before(function() {
+      return driver.manage().timeouts().setScriptTimeout(2000);
+    });
+
+    it('should set timeout pre-emptively and execute script', function() {
+      return driver.get('https://mozilla.org')
+      .then(() => {
+        return driver.executeAsyncScript(() => {
+          const callback = arguments[arguments.length - 1];
+          setTimeout(callback, 100)
+        });
+      })
+    });
+
+    it('should set timeout pre-emptively and execute script', function() {
+      return driver.get('https://mozilla.org')
+      .then(() => {
+        return driver.executeAsyncScript(() => {
+          const callback = arguments[arguments.length - 1];
+          setTimeout(callback, 2500)
+        });
+      }).then(() => {
+        assert.fail('it should have timed out');
+      }).catch((e) => {
+        assert.equal(e.name, 'ScriptTimeoutError');
+        assert.include(e.message, 'asynchronous script timeout: result was not \
+received in 2 seconds')
+      })
+    })
+
   })
 
 })
